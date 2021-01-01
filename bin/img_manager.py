bdca3430ff65babb64d05f1e5908915d1d068fcd
hanlimin <magicsli@outlook.com>
2021-01-01 18:07:30 +0800

    temp

--- bin/img_manager.py
+++ bin/img_manager.py
@@ -28,39 +28,37 @@ def md_img_find(md_file):
     将给定的markdown文件里的图片本地路径转换成网上路径
     """
 
-    post = None
+    text = None
     logger.info("文件名：", md_file)
     with open(md_file, "r", encoding=CHARSET) as f:
         global total, ignore
-        post = f.read()
-        matches = RE_MARKDOWN_IMG_URL_PATTERN.findall(post)
-        if matches and len(matches) > 0:
-            for sub_match in matches:
-                for match in sub_match:
-                    total = total + 1
-                    if match and len(match) > 0:
-                        logger.info(f"匹配路径 :{match} ")
-                        if not re.match(RE_URL_PATTREN, match):
-                            loc_p = match
-                            if not os.path.exists(loc_p) or not os.path.isfile(loc_p):
-                                loc_p = md_file[: md_file.rfind("\\") + 1] + match
-                            if os.path.exists(loc_p) and os.path.isfile(loc_p):
-                                # 如果是一个图片的话，才要上传，否则的话，不用管
-                                if isImg(loc_p):
-                                    pass
-                                else:
-                                    ignore = ignore + 1
-                                    logger.info(f"不是一个图片文件 ：{loc_p}")
-                                    continue
+        text = f.read()
+        matches = RE_MARKDOWN_IMG_URL_PATTERN.findall(text)
+        if matches:
+            for match in matches:
+                if match:
+                    logger.info(f"匹配路径 :{match} ")
+                    if RE_URL_PATTREN.match(match):
+                        loc_p = match
+                        if not os.path.exists(loc_p) or not os.path.isfile(loc_p):
+                            loc_p = md_file[: md_file.rfind("\\") + 1] + match
+                        if os.path.exists(loc_p) and os.path.isfile(loc_p):
+                            # 如果是一个图片的话，才要上传，否则的话，不用管
+                            if isImg(loc_p):
+                                pass
                             else:
                                 ignore = ignore + 1
-                                logger.info(f" 文件不存在 ：{loc_p}")
+                                logger.info(f"不是一个图片文件 ：{loc_p}")
+                                continue
                         else:
-                            logger.info(f"markdown文件中的图片用的是网址 ：{match}")
-                            file_url = transfer_online_img(match)
-                            if file_url:
-                                logger.info(f"图片地址是 ：{file_url}")
-                                # post = post.replace(match, file_url)
+                            ignore = ignore + 1
+                            logger.info(f" 文件不存在 ：{loc_p}")
+                    else:
+                        logger.info(f"本地地址：{match}")
+                        file_url = transfer_online_img(match)
+                        if file_url:
+                            logger.info(f"图片地址是 ：{file_url}")
+                            # post = post.replace(match, file_url)
 
 
 def main(args):
